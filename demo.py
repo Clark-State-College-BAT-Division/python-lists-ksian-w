4a57a67aa409bcaa91a00d5a65ca9391cc5c4745
ksian-w <kryspjeanlouis@gmail.com>
2025-10-03 17:35:09 -0700

    change

---
+++ demo.py
@@ -0,0 +1,16 @@
+daysOfWeek = ["Monday", "Tuesday", "Wednesdsay", "thursday", "Friday", "Saturday", "Sunday"]
+
+myData = []
+
+numbers = range(10)
+
+print(daysOfWeek[0])
+
+print(numbers[5])
+
+print(numbers [9])
+
+for i in range (7):
+    print(daysOfWeek[i])
+    for j in range(5):
+        print(numbers[j])
--- meanmedian.py
+++ meanmedian.py
@@ -3,3 +3,14 @@
 #Calculate and display the mean and median
 #This is a guided practice. You can follow the video or your instructor will go
 #over this in class
+
+myNumbers = [0,0,0,0,0]
+
+for i in range(5):
+    myNumbers[i] = int(input("Enter a number: "))
+
+print(myNumbers) #only doable in python in other languages you CANNOT declare it this way. 
+myNumbers.sort()
+print(myNumbers)
+myNumbers.sort(reverse=True)
+print(myNumbers)
